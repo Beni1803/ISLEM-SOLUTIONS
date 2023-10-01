340fdd6a08c82c2a2d4b1503bf7de05a67499f8d
Beni1803 <beni1803@usherbrooke.ca>
2023-10-01 14:16:41 -0400

    Removed the dashboard app

--- dashboard/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class WebApp(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
-    url = models.URLField()
--- dashboard/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import WebApp
-
-def dashboard_view(request):
-    apps = WebApp.objects.all()
-    context = {'apps': apps}
-    return render(request, 'dashboard/dashboard.html', context)
