1abdf829d0953f5072d7680404c24f8046a3e066
Beni1803 <beni1803@usherbrooke.ca>
2023-10-02 14:51:56 -0400

    Your commit message here

--- spectrum_finder/views.py
+++ spectrum_finder/views.py
@@ -31,38 +31,71 @@ def home(request):
     selected_band_blocks_grouped = {}
 
     if request.method == 'POST':
-        start_ul = request.POST.get('start_ul')
-        stop_ul = request.POST.get('stop_ul')
+        combined_inputs = request.POST.get('frequency_range').split()
         
-        try:
-            start_ul = float(start_ul.strip())
-            stop_ul = float(stop_ul.strip())
+        buffer = []
 
-            # Find matching band-blocks based on the input frequencies
-            for freq_band, bands in band_data_grouped.items():
-                for band in bands:
-                    try:
-                        # If Start_(UL) and Stop_(UL) are empty, use Start_(DL) and Stop_(DL) instead
-                        start_band_key = "Start_(UL)" if band["Start_(UL)"] else "Start_(DL)"
-                        stop_band_key = "Stop_(UL)" if band["Stop_(UL)"] else "Stop_(DL)"
+        for part in combined_inputs:
+            # If current part contains a dash, it's a range
+            if '-' in part:
+                buffer.append(part)
 
-                        start_band = float(band[start_band_key])
-                        stop_band = float(band[stop_band_key])
+                # If there's a `/`, it means we have both UL and DL in the buffer
+                if '/' in part:
+                    # Split the current part into UL and DL
+                    ul_input, dl_input = part.split('/')
+                    process_input(ul_input, dl_input, band_data_grouped, selected_band_blocks_grouped)
+                    buffer = []  # Clear buffer
 
-                        if start_band <= start_ul and stop_band >= stop_ul:
-                            if freq_band not in selected_band_blocks_grouped:
-                                selected_band_blocks_grouped[freq_band] = []
-                            selected_band_blocks_grouped[freq_band].append(band)
-                    except ValueError:
-                        messages.error(request, f'Failed to convert band values: {start_band_key}={band[start_band_key]}, {stop_band_key}={band[stop_band_key]}')
+                # If buffer has 2 elements without a `/`, it's two DL inputs
+                elif len(buffer) == 2:
+                    dl_input = buffer[0]
+                    process_input(dl_input, None, band_data_grouped, selected_band_blocks_grouped)
+                    buffer = [buffer[1]]  # Keep the last element for next iteration
 
             else:
-                count = sum(len(bands) for bands in selected_band_blocks_grouped.values())
-                messages.success(request, f'Found {count} band-blocks for the provided frequencies.')
+                messages.error(request, f'Invalid input detected: {part}')
 
-        except ValueError:
-            messages.error(request, f'Failed to convert values: start_ul={start_ul}, stop_ul={stop_ul}')
-            messages.error(request, 'Please provide valid numbers for the start and stop frequencies.')
+        # Handle any remaining items in the buffer
+        if buffer:
+            if len(buffer) == 1:
+                dl_input = buffer[0]
+                process_input(dl_input, None, band_data_grouped, selected_band_blocks_grouped)
+            else:
+                messages.error(request, f'Unexpected buffer state: {buffer}')
+        
+        count = sum(len(bands) for bands in selected_band_blocks_grouped.values())
+        messages.success(request, f'Found {count} band-blocks for the provided frequencies.')
+
     return render(request, 'spectrum_finder/home.html', {
         'selected_band_blocks_grouped': selected_band_blocks_grouped
     })
+
+def process_input(ul_input, dl_input, band_data_grouped, selected_band_blocks_grouped):
+    try:
+        if ul_input:
+            start_ul, stop_ul = [float(val) for val in ul_input.split('-')]
+        if dl_input:
+            start_dl, stop_dl = [float(val) for val in dl_input.split('-')]
+        if not ul_input:
+            start_ul, stop_ul = start_dl, stop_dl
+
+        # Find matching band-blocks based on the input frequencies
+        for freq_band, bands in band_data_grouped.items():
+            for band in bands:
+                try:
+                    # If Start_(UL) and Stop_(UL) are empty, use Start_(DL) and Stop_(DL) instead
+                    start_band_key = "Start_(UL)" if band["Start_(UL)"] else "Start_(DL)"
+                    stop_band_key = "Stop_(UL)" if band["Stop_(UL)"] else "Stop_(DL)"
+
+                    start_band = float(band[start_band_key])
+                    stop_band = float(band[stop_band_key])
+
+                    if start_band <= start_ul and stop_band >= stop_ul:
+                        if freq_band not in selected_band_blocks_grouped:
+                            selected_band_blocks_grouped[freq_band] = []
+                        selected_band_blocks_grouped[freq_band].append(band)
+                except ValueError:
+                    messages.error(request, f'Failed to convert band values: {start_band_key}={band[start_band_key]}, {stop_band_key}={band[stop_band_key]}')
+    except ValueError:
+        messages.error(request, f'Failed to convert input values: UL={ul_input}, DL={dl_input}')
